0d3e96d576a1a527a6242dc36fbfa16ae3f25a01
mobin ghaffari <www.80mobin@gmail.com>
2023-03-30 15:38:15 +0430

    Add project files.

---
+++ ChatGpt/Controllers/GPTController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+
+namespace ChatGpt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class GPTController : ControllerBase
+    {
+        [HttpGet]
+        [Route("UseChatGpt")]
+        public async Task<IActionResult> UseChatGpt(string query)
+        {
+            string result = "";
+            var openAi = new OpenAIAPI("YOUR API KEY");
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt=query;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+
+            var completions=openAi.Completions.CreateCompletionAsync(completionRequest);
+
+            foreach (var completion in completions.Result.Completions)
+            {
+                result += completion.Text;
+            }
+
+            return Ok(result);
+        }
+    }
+}
